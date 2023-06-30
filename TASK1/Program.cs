9d8cfdd06c2f68381f49c3148269d029e67ec727
Dopzhik <haltura24@gmail.com>
2023-06-30 17:41:52 +0800

    Решена 2 задача

---
+++ TASK1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    string inputedmesag = Console.ReadLine();
+    int number = Convert.ToInt32(inputedmesag);
+    return number;
+}
+bool Validate3Digits(int number)
+{
+    if (99 < number && number < 1000)   // 99 < number < 1000
+    {
+        return true;    // Проверка прошла успешно
+    }
+    System.Console.WriteLine("Число не трехзначное");    // проверка не прошла
+    return false;
+}
+int value = ReadInt("Введите трехзначное число => ");
+if (Validate3Digits(value))
+{
+    int seconddigit = (value / 10) % 10;
+    System.Console.WriteLine($"Вторая цифра из числа {value} равна {seconddigit}");
+}
---
+++ TASK2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    string inputedmesag = Console.ReadLine();
+    int number = Convert.ToInt32(inputedmesag);
+    return number;
+}
+bool Validate3Digits(int number)
+{
+    if (number > 99)   // условие что число больше 99
+    {
+        return true;    // Проверка прошла успешно
+    }
+    System.Console.WriteLine("В заданном числе третьей цифры нет");    // проверка не прошла
+    return false;
+}
+int[] GetIntArray(int number)
+{
+    List<int> listOfInts = new List<int>();
+    while(number > 0)
+    {
+        listOfInts.Add(number % 10);
+        number = number / 10;
+    }
+    listOfInts.Reverse();
+    return listOfInts.ToArray();
+}
+int value = ReadInt("Введите любое число => ");
+if (Validate3Digits(value))
+{
+    int[] listOfdigits = GetIntArray(value);
+    System.Console.Write($"Третья цифра из числа {value} равна {listOfdigits[2]}");
+}
